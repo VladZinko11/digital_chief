8f24f031505f93cb245cd650b398c7320b7521ee
VladZinko <zinko1996@yandex.ru>
2024-07-16 13:34:15 +0300

    Initial commit created project, with spring boot, spring data jpa, postgresql, maven

---
+++ src/main/java/com/digitalchief/zinko/digital_chief_test/DigitalChiefTestApplication.java
@@ -0,0 +1,13 @@
+package com.digitalchief.zinko.digital_chief_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalChiefTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalChiefTestApplication.class, args);
+    }
+
+}
