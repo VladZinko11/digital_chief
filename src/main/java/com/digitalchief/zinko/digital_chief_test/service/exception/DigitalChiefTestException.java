cc5018473c2cc5b21bb29fbcb629acf36f1036b0
VladZinko <zinko1996@yandex.ru>
2024-07-17 23:56:25 +0300

    created custom Exception class - DigitalChiefTestException that extend RuntimeException created NotFoundException extend DigitalChiefTestException

---
+++ src/main/java/com/digitalchief/zinko/digital_chief_test/service/exception/DigitalChiefTestException.java
@@ -0,0 +1,15 @@
+package com.digitalchief.zinko.digital_chief_test.service.exception;
+
+public class DigitalChiefTestException extends RuntimeException {
+    public DigitalChiefTestException() {
+        super();
+    }
+
+    public DigitalChiefTestException(String message) {
+        super(message);
+    }
+
+    public DigitalChiefTestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
