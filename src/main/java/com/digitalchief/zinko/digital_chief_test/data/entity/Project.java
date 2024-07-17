c9f30cdfe88d5706cae5474d56d8589a0423f99a
VladZinko <zinko1996@yandex.ru>
2024-07-17 23:35:46 +0300

    created entities Project and Task Project - OneToMany Task, created enum Type for type of Task

---
+++ src/main/java/com/digitalchief/zinko/digital_chief_test/data/entity/Project.java
@@ -0,0 +1,37 @@
+package com.digitalchief.zinko.digital_chief_test.data.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(orphanRemoval = true, mappedBy = "project", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OrderBy("project")
+    private List<Task> tasks;
+
+
+}
---
+++ src/main/java/com/digitalchief/zinko/digital_chief_test/data/entity/Type.java
@@ -0,0 +1,5 @@
+package com.digitalchief.zinko.digital_chief_test.data.entity;
+
+public enum Type {
+    DESIGN, MARKETING, BUSINESS_DEVELOPMENT, PROJECT_MANAGEMENT
+}
