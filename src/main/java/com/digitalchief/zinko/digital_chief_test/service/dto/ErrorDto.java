f397a814d9a7950f4e8e3da2e5a152c49c206589
VladZinko <zinko1996@yandex.ru>
2024-07-18 01:41:58 +0300

    created ErrorDto and its sub class ValidationResultDto

---
+++ src/main/java/com/digitalchief/zinko/digital_chief_test/service/dto/ErrorDto.java
@@ -0,0 +1,15 @@
+package com.digitalchief.zinko.digital_chief_test.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDto {
+
+    private String error;
+    private String message;
+}
